bdaf8d320b9af2d29534fbfabdec1c9711ed260f
SuarezJulian8 <130808190+SuarezJulian8@users.noreply.github.com>
2024-05-29 23:08:48 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,28 @@
+import streamlit as st
+import numpy as np
+import pickle
+import sklearn
+
+
+def tokenizer(text):
+  return text.split()
+
+st.title("Predict reviews' sentiment")
+
+review = st.text_input("Ingrese la reseña: ")
+review_array = np.array([review])
+
+try:
+    with open("model.pickle", "rb") as f:
+        model = pickle.load(f)
+    print("Archivo cargado correctamente.")
+except FileNotFoundError:
+    print("El archivo 'model.pickle' no se encontró. Verifica la ruta del archivo.")
+
+if st.button("Predicción"):
+    pred = model.predict(review_array)[0]
+
+    if pred == 0:
+        st.write("Negativa")
+    else:
+        st.write("Positiva")
